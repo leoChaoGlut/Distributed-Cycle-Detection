decadf26b5861d395f02050219103cd3da4ca4aa
liaochao.lc <liaochao.lc@alibaba-inc.com>
2019-04-08 15:42:49 +0800

    Mod[pseudo distributed] 1. 去除多余测试代码

--- src/main/java/personal/leo/dcd/impl/distributed/pseudo/PseudoDistributed.java
+++ src/main/java/personal/leo/dcd/impl/distributed/pseudo/PseudoDistributed.java
@@ -82,44 +82,6 @@ public class PseudoDistributed {
         }
     }
 
-    @Test
-    public void fewData() throws InterruptedException {
-        int workerCount = 2;
-        Worker w1 = new Worker(1);
-        Worker w2 = new Worker(2);
-
-        Vertex v1 = new Vertex(Id.next());
-        Vertex v2 = new Vertex(Id.next());
-        Vertex v3 = new Vertex(Id.next());
-        Vertex v4 = new Vertex(Id.next());
-        Vertex v5 = new Vertex(Id.next());
-
-        v1.out(v2);
-        v2.out(v3);
-        v3.out(v4).out(v5);
-        v4.out(v2);
-
-        VertexHolder.put(w1.getId(), Arrays.asList(v1, v2));
-        VertexHolder.put(w2.getId(), Arrays.asList(v3, v4, v5));
-
-        List<Worker> workers = Arrays.asList(w1, w2);
-
-        ExecutorService es = Executors.newFixedThreadPool(2);
-
-        int round = 0;
-        while (VertexHolder.anyActive()) {
-            CountDownLatch latch = new CountDownLatch(workerCount);
-
-            distributedDetect(es, round, latch);
-
-            //在分布式环境下,需要一个 coordinator 来控制所有 worker 何时走向下一轮,一致性可通过 db 保证.
-            latch.await();
-
-            round++;
-        }
-
-    }
-
     private void distributedDetect(ExecutorService es, int round, CountDownLatch latch) {
         for (Worker worker : workers) {
             worker.setRound(round);
@@ -127,13 +89,6 @@ public class PseudoDistributed {
 
             es.submit(worker);
         }
-    }
-
-    @Test
-    public void moreData() {
-        int workerCount = 10;
-        int vtxCount = 100_000;
-
     }
 
 }
